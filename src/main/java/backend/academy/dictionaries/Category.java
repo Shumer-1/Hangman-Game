784f7b173cc6baa6e239f918e7adf0fe6b6c8b8a
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-07 18:13:56 +0300

    feat(fields): add word categories

---
+++ src/main/java/backend/academy/dictionaries/Category.java
@@ -0,0 +1,12 @@
+package backend.academy.dictionaries;
+
+/**
+ * Категории слов для игровой сессии.
+ */
+public enum Category {
+    TOWNS,
+    ANIMALS,
+    NAMES,
+    COUNTRIES,
+    FRUITS,
+}
