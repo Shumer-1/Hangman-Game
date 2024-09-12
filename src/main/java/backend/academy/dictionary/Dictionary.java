ed261c95fe767683a7d63799eefa94ebd9a35797
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-12 21:43:55 +0300

    refactor(dictionary): reorganize class dictionary using DataExtractor interface

--- src/main/java/backend/academy/dictionary/Dictionary.java
+++ src/main/java/backend/academy/dictionary/Dictionary.java
@@ -1,12 +1,6 @@
 package backend.academy.dictionary;
 
 import backend.academy.exceptions.MissingDataException;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvValidationException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,11 +12,17 @@ public class Dictionary {
     private final static int BORDER_EASY = 4;
     private final static int BORDER_MEDIUM = 8;
     private final static List<Category> ALREADY_PRESENT = new ArrayList<>();
+    private final DataExtractor dataExtractor;
 
-    public static Kit getRandomWord(Category category, Difficulty difficulty) throws MissingDataException {
+    public Dictionary(DataExtractor dataExtractor) {
+        this.dataExtractor = dataExtractor;
+
+    }
+
+    public Kit getRandomWord(Category category, Difficulty difficulty) throws MissingDataException {
         fillOutDictionary(category);
 
-        if (DICTIONARY.get(category).get(difficulty).isEmpty()){
+        if (DICTIONARY.get(category).get(difficulty).isEmpty()) {
             throw new MissingDataException("File hasn't words with this difficulty");
         }
 
@@ -32,48 +32,23 @@ public class Dictionary {
         return DICTIONARY.get(category).get(difficulty).get(randomValue);
     }
 
-    private static CSVReader openDictionaryFile(String path){
-        CSVReader reader;
-        try {
-            reader = new CSVReaderBuilder(new FileReader(path)).build();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return reader;
-    }
-
-    private static void fillOutDictionary(Category category){
-        if (ALREADY_PRESENT.contains(category)){
+    private void fillOutDictionary(Category category) {
+        if (ALREADY_PRESENT.contains(category)) {
             return;
         }
 
         String path = "src/main/resources/" + category.toString().toLowerCase() + ".csv";
-        try (CSVReader reader = openDictionaryFile(path)) {
-            String[] nextLine;
-            reader.skip(1);
-            Map<Difficulty, List<Kit>> values = new HashMap<>();
+        List<Kit> kits = dataExtractor.load(path);
 
-            values.put(Difficulty.EASY, new ArrayList<>());
-            values.put(Difficulty.MEDIUM, new ArrayList<>());
-            values.put(Difficulty.HARD, new ArrayList<>());
+        Map<Difficulty, List<Kit>> values = new HashMap<>();
+        values.put(Difficulty.EASY, kits.stream().filter(kit -> kit.word().length() <= BORDER_EASY).toList());
+        values.put(Difficulty.MEDIUM,
+            kits.stream().filter(kit -> kit.word().length() <= BORDER_MEDIUM && kit.word().length() > BORDER_EASY)
+                .toList());
+        values.put(Difficulty.HARD, kits.stream().filter(kit -> kit.word().length() > BORDER_MEDIUM).toList());
 
-            while ((nextLine = reader.readNext()) != null) {
-                Difficulty difficulty = Difficulty.HARD;
-                if (nextLine[0].length() <= BORDER_EASY){
-                    difficulty = Difficulty.EASY;
-                }
-                else if (nextLine[0].length() <= BORDER_MEDIUM){
-                    difficulty = Difficulty.MEDIUM;
-                }
-                values.get(difficulty).add(new Kit(nextLine[0], nextLine[1]));
-            }
-            DICTIONARY.put(category, values);
-            ALREADY_PRESENT.add(category);
-
-        } catch (IOException | CsvValidationException e) {
-            throw new RuntimeException(e);
-        }
-
+        DICTIONARY.put(category, values);
+        ALREADY_PRESENT.add(category);
     }
 }
 
