107e9abc0fc1e621cb4408c0c67f7869b93ef5f5
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-13 11:55:31 +0300

    feat(exceptions): add new exception for wrong input value

---
+++ src/main/java/backend/academy/exceptions/WrongInputValueException.java
@@ -0,0 +1,7 @@
+package backend.academy.exceptions;
+
+public class WrongInputValueException extends Exception {
+    public WrongInputValueException(String message) {
+        super(message);
+    }
+}
