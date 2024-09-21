961d7a3f6bf0fa0b256cc9a7b34bf4500e6a714a
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-21 18:56:53 +0300

    feat(console): add class that reads data from console

---
+++ src/main/java/backend/academy/console/InputReader.java
@@ -0,0 +1,49 @@
+package backend.academy.console;
+
+import backend.academy.dictionary.Category;
+import backend.academy.dictionary.Difficulty;
+import backend.academy.dictionary.RandomTaker;
+import backend.academy.exceptions.WrongInputValueException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class InputReader {
+    private final Scanner scanner;
+
+    public InputReader(InputStream inputStream) {
+        scanner = new Scanner(inputStream);
+    }
+
+    public Category getCategory() {
+        Category category;
+        category = switch (scanner.nextLine()) {
+            case "animals" -> Category.ANIMALS;
+            case "towns" -> Category.TOWNS;
+            case "countries" -> Category.COUNTRIES;
+            case "fruits" -> Category.FRUITS;
+            case "names" -> Category.NAMES;
+            default -> new RandomTaker().takeRandomCategory();
+        };
+        return category;
+    }
+
+    public Difficulty getDifficulty() {
+        Difficulty difficulty;
+        difficulty = switch (scanner.nextLine()) {
+            case "easy" -> Difficulty.EASY;
+            case "medium" -> Difficulty.MEDIUM;
+            case "hard" -> Difficulty.HARD;
+            default -> new RandomTaker().takeRandomDifficulty();
+        };
+        return difficulty;
+    }
+
+    public String getLetter() throws WrongInputValueException {
+        String inputValue = scanner.nextLine();
+        if (inputValue.length() == 1 && Pattern.matches("[a-zA-Z]", inputValue)) {
+            return inputValue;
+        }
+        throw new WrongInputValueException("Input value must be letter");
+    }
+}
