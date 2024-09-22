6a18f6bb027b8829518e6de25211bfe72043e25d
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-22 18:58:13 +0300

    feat(gameLogic): add game logic class

---
+++ src/main/java/backend/academy/gameLogic/GameLogicUtil.java
@@ -0,0 +1,18 @@
+package backend.academy.gameLogic;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class GameLogicUtil {
+
+    public static boolean gameStep(String letter, String gameWord, StringBuilder guessedWord) {
+        boolean flag = false;
+        for (int i = 0; i < gameWord.length(); i++) {
+            if (gameWord.charAt(i) == letter.charAt(0)) {
+                flag = true;
+                guessedWord.setCharAt(i, letter.charAt(0));
+            }
+        }
+        return flag;
+    }
+}
