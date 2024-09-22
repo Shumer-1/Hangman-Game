a77c5cc1b1bd81950aa920ae1157d35c32dcab10
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-22 19:00:04 +0300

    feat(Main): fill main method

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,21 @@
 package backend.academy;
 
+import backend.academy.console.ConsoleWriter;
+import backend.academy.console.InputReader;
+import backend.academy.dictionary.CSVDataExtractor;
+import backend.academy.dictionary.Dictionary;
+import backend.academy.exceptions.MissingDataException;
+import backend.academy.sessions.GameSession;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MissingDataException {
+        var consoleWriter = new ConsoleWriter(System.out);
+        var inputReader = new InputReader(System.in);
+        var dictionary = new Dictionary(new CSVDataExtractor());
+
+        var gameSession = new GameSession(consoleWriter, inputReader, dictionary);
+        gameSession.startSession();
     }
 }
