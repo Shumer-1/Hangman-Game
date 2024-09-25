585568b479dac85ac7bd16681965b783e4ac09ab
Shumer-1 <borisov1908vlad@mail.ru>
2024-09-25 12:57:45 +0300

    test(gameLogic): add game logic test

---
+++ src/test/java/backend/academy/gameLogic/GameLogicUtilTest.java
@@ -0,0 +1,31 @@
+package backend.academy.gameLogic;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class GameLogicUtilTest {
+
+    @ParameterizedTest
+    @MethodSource("getArgumentsForGameStepTest")
+    void gameStepTest(String letter, String gameWord, StringBuilder guessedWord, boolean expectedResult, String expectedWord){
+        boolean actualResult = GameLogicUtil.gameStep(letter, gameWord, guessedWord);
+
+        assertAll(
+            () -> assertThat(actualResult).isEqualTo(expectedResult),
+            () -> assertThat(guessedWord.toString()).isEqualTo(expectedWord)
+        );
+    }
+
+    private static Stream<Arguments> getArgumentsForGameStepTest(){
+        return Stream.of(
+            Arguments.of("a", "arguments", new StringBuilder("_________"), true, "a________"),
+            Arguments.of("b", "arguments", new StringBuilder("_________"), false, "_________"),
+            Arguments.of("a", "aaaa", new StringBuilder("____"), true, "aaaa")
+        );
+    }
+
+}
