eadf91225af8da535bf5426854ae92b99aa8e744
Shumer-1 <borisov1908vlad@mail.ru>
2024-10-11 16:30:22 +0300

    refactor(Category): update enum

--- src/main/java/backend/academy/dictionary/Category.java
+++ src/main/java/backend/academy/dictionary/Category.java
@@ -1,24 +1,15 @@
 package backend.academy.dictionary;
 
-import java.util.Arrays;
-
 public enum Category {
-    TOWNS(0),
-    ANIMALS(1),
-    NAMES(2),
-    COUNTRIES(3),
-    FRUITS(4);
+    TOWNS,
+    ANIMALS,
+    NAMES,
+    COUNTRIES,
+    FRUITS;
 
-    private final int number;
-
-    Category(int number) {
-        this.number = number;
-    }
+    private static final Category[] CACHED_VALUES = Category.values();
 
     public static Category getByNumber(int number) {
-        return Arrays.stream(values())
-            .filter(it -> it.number == number)
-            .findFirst().orElseThrow(() -> new IllegalArgumentException("Wrong number of category"));
+        return CACHED_VALUES[number];
     }
-
 }
